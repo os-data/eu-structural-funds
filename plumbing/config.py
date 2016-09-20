a2da73168c29aa4af5f59986de77993339f87a58
cyberbikepunk <loic.jounot@gmail.com>
2016-09-20 16:30:19 +0200

    Sandbox the bootstrap module (part 3).
    - rename update.py -> bootstrap.py
    - change object imports to module imports to improve readability
    - refactor some of the functions
    - add a docstring to the module
    - explicitly specify that the module on;y supports python3

--- plumbing/config.py
+++ plumbing/config.py
@@ -1,15 +1,13 @@
-"""Plumbing configuration."""
+"""Pipeline configuration parameters."""
 
 from os.path import dirname, abspath, join
-from logging import info
 
+
+DATAPACKAGE_FILE = 'source.description.json'
+PIPELINE_SPECS = 'pipeline-specs.yaml'
+DESCRIPTION_FILE = 'source.description.yaml'
 
 ROOT_DIR = abspath(join(dirname(__file__), '..'))
 PIPELINE_TEMPLATE = join(ROOT_DIR, 'plumbing', 'pipeline.template.yaml')
-DATAPACKAGE_FILE = 'source.description.json'
-PIPELINE_SPECS = 'pipeline-specs.yaml'
-GEOCODE_COLUMN = 4
 LOG_FILE = join(ROOT_DIR, 'plumbing', 'log', 'plumbing.log')
-
-
-# info('Root folder: %s', ROOT_DIR)
+GEOCODES_FILE = join(ROOT_DIR, 'geography', 'geocodes.nuts.csv')
