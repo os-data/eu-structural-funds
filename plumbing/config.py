9e5ab2a04efbac056e47703fb5711bbe66a8f6cb
cyberbikepunk <loic.jounot@gmail.com>
2016-09-23 10:34:12 +0200

    Refactor the bootstrap module.
    This version addresses the previous comments. It also has better focused functions and clarifies the top level control flow. And now pipeline-specs.yaml files are written according to specs :-).

--- plumbing/config.py
+++ plumbing/config.py
@@ -3,24 +3,14 @@
 from os.path import dirname, abspath, join
 
 
-PIPELINE_FILE = 'pipeline-specs.yaml'
+PIPELINE_FILE = 'pipeline-spec.yaml'
 DATAPACKAGE_FILE = 'source.datapackage.json'
 DESCRIPTION_FILE = 'source.description.yaml'
+EXTRACTOR_FILE = 'scraper.py'
 
 ROOT_DIR = abspath(join(dirname(__file__), '..'))
 PIPELINE_TEMPLATE = join(ROOT_DIR, 'plumbing', 'pipeline.template.yaml')
 LOG_FILE = join(ROOT_DIR, 'plumbing', 'log', 'plumbing.log')
 GEOCODES_FILE = join(ROOT_DIR, 'geography', 'geocodes.nuts.csv')
-
 VALIDATOR_PROCESSOR = 'validate_datapackage'
-
-DEFAULT_PIPELINE = {
-    'schedule': {
-        'crontab': '@annually',
-    },
-    'pipeline': {
-        'run': VALIDATOR_PROCESSOR,
-        'params': DATAPACKAGE_FILE
-    }
-}
-
+DEFAULT_PIPELINE = {'schedule': {'crontab': '0 0 1 1 *'}, 'pipeline': []}
