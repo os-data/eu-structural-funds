efa3f78ae6e8351a71e5cbde01d58bda799ecd3c
Fernando Blat <ferblape@gmail.com>
2016-09-24 10:57:40 +0200

    Dump projects in CSV

--- data/MT.malta/scraper.py
+++ data/MT.malta/scraper.py
@@ -1,57 +1,84 @@
 import requests
 from lxml import html
 from pprint import pprint
+import csv
 
 # Base URL is the host of the page
 BASE_URL = 'https://investinginyourfuture.gov.mt'
 # Projects are fetch from the paginated list
 PAGINATION_URL = 'https://investinginyourfuture.gov.mt/ajax/loadProjects.ashx?page='
 
+
 def get_text(html_node):
     if html_node is not None:
         return html_node.text
 
+
 def scrape_project(url):
-    data = {}
+    project_data = []
     url = BASE_URL + url
 
     res = requests.get(url)
     doc = html.fromstring(res.content)
-    data['Code'] = get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectRefCode"]'))
-    data['Title'] = get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectTitle"]'))
-    data['Project Cost'] = get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectCostBeneficiaryItem_divCostValue"]'))
-    data['Beneficiary'] = get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectCostBeneficiaryItem_divBeneficiaryValue"]'))
-    data['Line Ministry'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdLineMinistry"]'))
-    data['Start Date'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdStartDate"]'))
-    data['End Date'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdEndDate"]'))
-    data['Non Technical Short Summary Of Project'] = get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divNonTechnicalShortSummaryContent"]/p'))
-    data['Operational Programme'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdOperationalProgramme"]'))
-    data['Fund'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdFund"]'))
-    data['Operational Objective'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdOperationalObjective"]'))
-    data['Priority Axis'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdPriorityAxis"]'))
-    data['Focus Area Of Intervention'] = get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdFocusAreaOfIntervention1"]'))
-    data['Project Objectives'] = get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divProjectObjectives"]/p'))
-    data['Project Results'] = get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divProjectResults"]/p'))
-    data['Project Purpose'] = get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divProjectPurpose"]/p'))
+    # Code
+    project_data.append(get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectRefCode"]')))
+    # Title
+    project_data.append(get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectTitle"]')))
+    # Project Cost
+    project_data.append(get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectCostBeneficiaryItem_divCostValue"]')))
+    # Beneficiary
+    project_data.append(get_text(doc.find('.//span[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectCostBeneficiaryItem_divBeneficiaryValue"]')))
+    # Line Ministry
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdLineMinistry"]')))
+    # Start Date
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdStartDate"]')))
+    # End Date
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdEndDate"]')))
+    # Non Technical Short Summary Of Project
+    project_data.append(get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divNonTechnicalShortSummaryContent"]/p')))
+    # Operational Programme
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdOperationalProgramme"]')))
+    # Fund
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdFund"]')))
+    # Operational Objective
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdOperationalObjective"]')))
+    # Priority Axis
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdPriorityAxis"]')))
+    # Focus Area Of Intervention
+    project_data.append(get_text(doc.find('.//td[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_tdFocusAreaOfIntervention1"]')))
+    # Project Objectives
+    project_data.append(get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divProjectObjectives"]/p')))
+    # Project Results
+    project_data.append(get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divProjectResults"]/p')))
+    # Project Purpose
+    project_data.append(get_text(doc.find('.//div[@id="mainPlaceHolder_coreContentPlaceHolder_mainContentPlaceHolder_projectDetails_divProjectPurpose"]/p')))
 
-    pprint(data)
+    return project_data
 
 
 def scrape():
-    page = 1
-    while True:
-        res = requests.get(PAGINATION_URL + str(page))
-        if 'Content-Length' in res.headers and res.headers['Content-Length'] == '0':
-            print("Exiting...")
-            break
+    headers = [ 'Code', 'Title', 'Project Cost', 'Beneficiary', 'Line Ministry', 'Start Date', 'End Date', 'Non Technical Short Summary Of Project', 'Operational Programme', 'Fund', 'Operational Objective',
+                'Priority Axis', 'Focus Area Of Intervention', 'Project Objectives', 'Project Results', 'Project Purpose' ]
 
-        doc = html.fromstring(res.content)
+    with open('data.csv', 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(headers)
 
-        for link in doc.findall('.//div[@class="project-listing-item-title"]/a'):
-            scrape_project(link.get('href'))
+        page = 28
+        while True:
+            res = requests.get(PAGINATION_URL + str(page))
+            if 'Content-Length' in res.headers and res.headers['Content-Length'] == '0':
+                print("Exiting...")
+                break
 
-        page = page + 1
-        print("\n")
+            doc = html.fromstring(res.content)
+
+            for link in doc.findall('.//div[@class="project-listing-item-title"]/a'):
+                project_data_row = scrape_project(link.get('href'))
+                writer.writerow(project_data_row)
+
+            page = page + 1
+            print("\n")
 
 
 if __name__ == '__main__':
