c66135902386976d145ab8587861ce710f5f30e0
cyberbikepunk <loic.jounot@gmail.com>
2016-10-04 20:14:11 +0200

    Add a utilities module with tests.
    The config module is also modified.

--- plumbing/config.py
+++ plumbing/config.py
@@ -2,6 +2,8 @@
 
 from os.path import dirname, abspath, join
 
+OS_TYPES_URL = ('https://raw.githubusercontent.com/'
+                'openspending/os-types/master/src/os-types.json')
 
 PIPELINE_FILE = 'pipeline-spec.yaml'
 DATAPACKAGE_FILE = 'source.datapackage.json'
@@ -14,3 +16,10 @@ LOG_FILE = join(ROOT_DIR, 'plumbing', 'log', 'plumbing.log')
 GEOCODES_FILE = join(ROOT_DIR, 'geography', 'geocodes.nuts.csv')
 VALIDATOR_PROCESSOR = 'validate_datapackage'
 DEFAULT_PIPELINE = {'schedule': {'crontab': '0 0 1 1 *'}, 'pipeline': []}
+
+SPECIFICATIONS_DIR = join(ROOT_DIR, 'specifications')
+CODELISTS_DIR = join(ROOT_DIR, 'codelists')
+
+FISCAL_SCHEMA_FILE = join(SPECIFICATIONS_DIR, 'fiscal.schema.yaml')
+FISCAL_MODEL_FILE = join(SPECIFICATIONS_DIR, 'fiscal.model.yaml')
+FISCAL_DATAPACKAGE_FILE = join(SPECIFICATIONS_DIR, 'fiscal.datapackage.yaml')
---
+++ plumbing/utilities.py
@@ -0,0 +1,55 @@
+"""A place for useful functions and classes that don't have a home."""
+
+import os
+import yaml
+
+from datapackage import DataPackage
+from plumbing.config import (
+    CODELISTS_DIR,
+    FISCAL_SCHEMA_FILE,
+    FISCAL_DATAPACKAGE_FILE,
+    FISCAL_MODEL_FILE
+)
+
+
+def get_all_codelists():
+    """Return all codelists as a dictionary of dictionaries."""
+
+    codelists = {}
+
+    for codelist_file in os.listdir(CODELISTS_DIR):
+        codelist_name, _ = os.path.splitext(codelist_file)
+        codelist = get_codelist(codelist_name)
+        codelists.update({codelist_name: codelist})
+
+    return codelists
+
+
+def get_codelist(codelist_file):
+    """Return one codelist as a dictionary."""
+
+    filepath = os.path.join(CODELISTS_DIR, codelist_file + '.yaml')
+    with open(filepath) as stream:
+        text = stream.read()
+        return yaml.load(text)
+
+
+def get_fiscal_datapackage(skip_validation=False):
+    """Create the master fiscal datapackage from parts."""
+
+    with open(FISCAL_DATAPACKAGE_FILE) as stream:
+        text = stream.read()
+        datapackage = yaml.load(text)
+
+    with open(FISCAL_SCHEMA_FILE) as stream:
+        text = stream.read()
+        datapackage['resources'][0]['schema'] = yaml.load(text)
+
+    with open(FISCAL_MODEL_FILE) as stream:
+        text = stream.read()
+        datapackage['model'] = yaml.load(text)
+
+    if not skip_validation:
+        DataPackage(datapackage, schema='fiscal').validate()
+
+    return datapackage
