f1fdfdc23d26e9b713211179a1ec393ed2b2800a
cyberbikepunk <loic.jounot@gmail.com>
2016-10-06 13:01:32 +0200

    Delete 1,  modify 1 and add 5 common processors.
        - read_description: this part of the code belonged to the bootstrap module but it's better as a processor. It reads a description file and converts it to a proper datapackage. One difference: it doesn't save it to file anymore, just passes the it through the pipeline. That way there's no confusion as to what is the source of truth.
        - stream_from_path: ingest a local file, now using  the`Stream` class. This processor doesn't require any datapackage descriptor.
        - update_fields:  update the datapackage object. The data can be validated against the schema  at this stage.
        - map_columns: apply column mapping specified by the `maps_to` property in the source datapackage.
        - reshape_data: pad the data table with missing columns (required by FDP)
        -  load_fiscal_datapackage: replace whatever datapackage descriptor is currently in the pipeline with the "master" FDP.
    
        Also:
    
        - add a `get_fiscal_fields` function in utilities because it's used by several processors
        - add a test module for the `reshape_data` processor. More tests needed.

---
+++ common/read_description.py
@@ -0,0 +1,48 @@
+"""A processor to convert the description file into a datapackage.
+
+The conversion
+    - 1) adds `name` fields in the metadata and in each resource
+    - 2) duplicates fields from the first resource to other resources if they
+         are missing (so that the description file can stay "dry")
+
+"""
+
+import json
+import yaml
+
+from slugify import slugify
+from datapackage_pipelines.wrapper import spew, ingest
+from plumbing.config import DESCRIPTION_FILE, DATAPACKAGE_FILE
+
+
+def create_datapackage(description):
+    description['name'] = slugify(description['title'], separator='-')
+    first_resource = description['resources'][0]
+
+    for resource in description['resources']:
+        for property_ in first_resource.keys():
+            if property_ not in resource:
+                resource[property_] = first_resource[property_]
+        resource['name'] = slugify(resource['title'])
+
+    return description
+
+
+def load_description_file():
+    with open(DESCRIPTION_FILE) as stream:
+        return yaml.load(stream.read())
+
+
+def save_datapackage_file(description):
+    with open(DATAPACKAGE_FILE, 'w+') as stream:
+        stream.write(json.dumps(description, indent=4))
+
+
+if __name__ == '__main__':
+    parameters, _, _ = ingest()
+    save_ = parameters['save_datapackage']
+    description_ = load_description_file()
+    datapackage = create_datapackage(description_)
+    if save_:
+        save_datapackage_file(description_)
+    spew(datapackage, [])
