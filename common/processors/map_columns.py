aebd2106b0725f111e011ac77e7b9769da96b037
cyberbikepunk <loic.jounot@gmail.com>
2016-10-14 12:14:21 +0200

    Refactor the `map_columns` processor.
    The processor is more robust to corner cases (see docstring) and now used the `process` helper function. Test have been added.

--- common/processors/map_columns.py
+++ common/processors/map_columns.py
@@ -1,46 +1,51 @@
-"""Map the raw columns names to fiscal fields where possible."""
+"""Map raw field names to fiscal field names where possible.
 
-import logging
+The mapping is determined by the property `maps_to` in the description file.
+Since the general assumption is that all resources in the description file
+have the exactly the same data structure, we just grab the mapping for the
+first resource.
+
+When building the lookup table, we check that:
+    1. The property `maps_to` exists
+    2. It's not empty
+    2. Its value matches a fiscal field
+
+"""
+
+from logging import info
 from datapackage_pipelines.wrapper import ingest, spew
-from common.utilities import get_fiscal_fields
+from common.utilities import get_fiscal_fields, process
 
 
-def get_column_mappings(datapackage, fiscal_fields):
-    """Return a lookup table matching old to new field names.
-    """
-    for resource in datapackage['resources']:
-        mapping = {
-            field['name']: field['maps_to']
-            for field in resource['schema']['fields']
-            if field['maps_to'] in fiscal_fields
-            }
-        logging.info('Resource = %s', resource['name'])
-        logging.info('Column map = %s', mapping)
-        yield mapping
+def build_lookup_table(datapackage_):
+    """Return the mapping for the first resource."""
+
+    fields = datapackage_['resources'][0]['schema']['fields']
+    fiscal_fields = get_fiscal_fields()
+
+    for field in fields:
+        checks = ('maps_to' in field,
+                  field.get('maps_to'),
+                  field.get('maps_to') in fiscal_fields)
+
+        if all(checks):
+            yield field['name'], field['maps_to']
+            info('%s mapped to %s', field['name'], field['maps_to'])
+        else:
+            info('Skipping %s', field['name'])
 
 
-def process_row(row, mapping):
-    """Rename the row keys according to the mapping.
-    """
-    for old, new in mapping.items():
-        row[new] = row.pop(old)
+def apply_mapping(row, mapping=None):
+    """Apply the mapping to one row."""
+
+    if mapping:
+        for old, new in mapping:
+            row[new] = row.pop(old)
     return row
 
 
-def process_resources(resources, mappings):
-    """Return an iterator of row iterators.
-    """
-    for resource, mapping in zip(resources, mappings):
-        def process_rows(resource_):
-            for row in resource_:
-                yield process_row(row, mapping)
-
-        yield process_rows(resource)
-
-
 if __name__ == '__main__':
-    _, datapackage_, resources_ = ingest()
-    fiscal_fields_ = get_fiscal_fields()
-    mappings_ = get_column_mappings(datapackage_, fiscal_fields_)
-    new_resources_ = process_resources(resources_, mappings_)
-    spew(datapackage_, new_resources_)
+    _, datapackage, resources = ingest()
+    lookup_table = build_lookup_table(datapackage)
+    new_resources = process(resources, apply_mapping, mapping=lookup_table)
+    spew(datapackage, new_resources)
