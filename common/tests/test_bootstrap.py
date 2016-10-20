321e440eb84fca7474df434dac686db7e4ca649b
cyberbikepunk <loic.jounot@gmail.com>
2016-10-20 16:57:04 +0200

    Move the tests folder to the top level.

--- common/tests/test_bootstrap.py
+++
@@ -1,9 +0,0 @@
-"""Test for the bootstrap module."""
-
-from common.bootstrap import collect_sources
-from pytest import mark
-
-
-@mark.parametrize('source', collect_sources())
-def test_source_description_files_are_valid(source):
-    assert not source.validation_errors
