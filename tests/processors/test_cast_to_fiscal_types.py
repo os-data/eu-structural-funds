eeb780be2096d015f3c79a5361771b31d5036c58
cyberbikepunk <loic.jounot@gmail.com>
2016-10-25 22:47:32 +0200

    Fix `cast_to_fiscal_types` units tests.

--- tests/processors/test_cast_to_fiscal_types.py
+++ tests/processors/test_cast_to_fiscal_types.py
@@ -40,34 +40,34 @@ type_examples = [
 @mark.parametrize('value', valid_dates)
 def test_cast_values_casts_dates_correctly(value):
     expected = {'approval_date': date(2000, 12, 31)}
-    assert cast_values({'approval_date': value}, fiscal_types) == expected
+    assert cast_values({'approval_date': value}) == expected
 
 
 @mark.parametrize('value', invalid_dates)
 def test_cast_values_returns_bad_guess(value):
     expected = {'approval_date': date(2000, 12, 31)}
-    assert cast_values({'approval_date': value}, fiscal_types) != expected
+    assert cast_values({'approval_date': value}) != expected
 
 
 def test_cast_values_handles_date_objects_correctly():
     expected = {'approval_date': date(2000, 12, 31)}
     date_object = {'approval_date': date(2000, 12, 31)}
-    assert cast_values(date_object, fiscal_types) == expected
+    assert cast_values(date_object) == expected
 
 
 @mark.parametrize('value', numbers)
 def test_cast_values_casts_to_numbers_correctly(value):
     expected = {'total_amount': 1.0}
-    assert cast_values({'total_amount': value}, fiscal_types) == expected
+    assert cast_values({'total_amount': value}) == expected
 
 
 @mark.parametrize('value, expected', strings)
 def test_cast_values_casts_to_strings_correctly(value, expected):
     expected_row = {'fund_name': expected}
-    assert cast_values({'fund_name': value}, fiscal_types) == expected_row
+    assert cast_values({'fund_name': value}) == expected_row
 
 
 @mark.parametrize('field_name', type_examples)
 def test_cast_values_handles_empty_cells_correctly(field_name):
     empty_cell = {field_name: None}
-    assert cast_values(empty_cell, fiscal_types) == empty_cell
+    assert cast_values(empty_cell) == empty_cell
