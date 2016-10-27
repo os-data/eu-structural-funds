4b63f66403c893a112cb647c8900b2aa34f381cb
cyberbikepunk <loic.jounot@gmail.com>
2016-10-27 15:05:18 +0200

    Expand the fiscal datapackage  in the log stream.
    Expand the JSON string to multiple lines for better readability.

--- common/processors/load_fiscal_datapackage.py
+++ common/processors/load_fiscal_datapackage.py
@@ -1,13 +1,16 @@
 """Grab the source description and convert it into a datapackage"""
 
+import json
 import logging
 
 from datapackage_pipelines.wrapper import ingest, spew
+from common.config import JSON_FORMAT
 from common.utilities import get_fiscal_datapackage
 
+
 if __name__ == '__main__':
-    # noinspection PyRedeclaration
     _, datapackage, resources = ingest()
     fiscal_datapackage = get_fiscal_datapackage(source=datapackage)
-    logging.debug('Datapackage = %s', datapackage)
+    fiscal_datapackage_as_json = json.dumps(fiscal_datapackage, **JSON_FORMAT)
+    logging.debug('Loaded fiscal datapackage:\n%s', fiscal_datapackage_as_json)
     spew(fiscal_datapackage, resources)
