9865b9a40526463f5b07f635ffd00c98b23121f0
cyberbikepunk <loic.jounot@gmail.com>
2016-12-06 09:43:11 +0200

    Refactor global _DATAPACKAGE  into fixture.

--- tests/processors/test_map_fields.py
+++ tests/processors/test_map_fields.py
@@ -1,6 +1,8 @@
 """Unit-tests for the `map_fields` processor."""
 
 from copy import deepcopy
+
+from pytest import fixture
 from pytest import mark
 from pytest import raises
 
@@ -11,47 +13,51 @@ from common.processors.map_fields import (
     apply_mapping,
     check_mapping_target)
 
-_DATAPACKAGE = {
-    'resources': [
-        {
-            'schema':
+
+@fixture(scope='function')
+def _datapackage():
+    return {
+        'resources': [
             {
-                'fields': [
-                    {'name': 'bacon'},
-                    {'name': 'foo', 'maps_to': FISCAL_KEYS[0]},
-                    {'name': 'bar', 'maps_to': '_ignored'},
-                    {'name': 'baz', 'maps_to': None},
-                    {'name': 'spam', 'maps_to': ''},
-                    {'name': 'eggs', 'maps_to': [FISCAL_KEYS[1],
-                                                 FISCAL_KEYS[2]]}
-                ]
+                'schema':
+                    {
+                        'fields': [
+                            {'name': 'bacon'},
+                            {'name': 'foo', 'maps_to': FISCAL_KEYS[0]},
+                            {'name': 'bar', 'maps_to': '_ignored'},
+                            {'name': 'baz', 'maps_to': None},
+                            {'name': 'spam', 'maps_to': ''},
+                            {'name': 'eggs', 'maps_to': [FISCAL_KEYS[1],
+                                                         FISCAL_KEYS[2]]}
+                        ]
+                    }
             }
-        }
-    ]
-}
+        ]
+    }
+
 
 # Test the mapping lookup table
 # -----------------------------------------------------------------------------
 
-_VALID_MAPPINGS = [
+_valid_mappings = [
     FISCAL_KEYS[0],
     [FISCAL_KEYS[1], FISCAL_KEYS[2]],
 ]
 
 
-@mark.parametrize('valid_mapping', _VALID_MAPPINGS)
+@mark.parametrize('valid_mapping', _valid_mappings)
 def test_check_mapping_asserts_error_on_invalid_mapping(valid_mapping):
     assert isinstance(check_mapping_target(valid_mapping), type(valid_mapping))
 
 
-_INVALID_MAPPINGS = [
+_invalid_mappings = [
     'bad',
     ['very_bad', 'worse'],
     [FISCAL_KEYS[0], 'extra_worse']
 ]
 
 
-@mark.parametrize('invalid_mapping', _INVALID_MAPPINGS)
+@mark.parametrize('invalid_mapping', _invalid_mappings)
 def test_check_mapping_asserts_error_on_invalid_mapping(invalid_mapping):
     with raises(AssertionError):
         check_mapping_target(invalid_mapping)
@@ -60,22 +66,25 @@ def test_check_mapping_asserts_error_on_invalid_mapping(invalid_mapping):
 # Test the mapping lookup table
 # -----------------------------------------------------------------------------
 
-def test_build_mapping_tables_returns_a_list_of_dicts():
-    mappings = build_mapping_tables(_DATAPACKAGE)
+# noinspection PyShadowingNames
+def test_build_mapping_tables_returns_a_list_of_dicts(_datapackage):
+    mappings = build_mapping_tables(_datapackage)
     assert isinstance(mappings, list)
     for mapping in mappings:
         assert isinstance(mapping, dict)
 
 
-def test_build_mapping_tables_output_has_correct_size():
-    mappings = build_mapping_tables(_DATAPACKAGE)
-    assert len(mappings) == len(_DATAPACKAGE['resources'])
-    for mapping, resource in zip(mappings, _DATAPACKAGE['resources']):
+# noinspection PyShadowingNames
+def test_build_mapping_tables_output_has_correct_size(_datapackage):
+    mappings = build_mapping_tables(_datapackage)
+    assert len(mappings) == len(_datapackage['resources'])
+    for mapping, resource in zip(mappings, _datapackage['resources']):
         assert len(mapping) == len(resource['schema']['fields'])
 
 
-def test_build_mapping_tables_returns_correct_mappings():
-    assert build_mapping_tables(_DATAPACKAGE) == [
+# noinspection PyShadowingNames
+def test_build_mapping_tables_returns_correct_mappings(_datapackage):
+    assert build_mapping_tables(_datapackage) == [
         {
             'bacon': None,
             'foo': FISCAL_KEYS[0],
@@ -90,16 +99,18 @@ def test_build_mapping_tables_returns_correct_mappings():
 # Test datapackage mutation
 # -----------------------------------------------------------------------------
 
-def test_update_datapackage_deletes_maps_to_property():
-    mappings = build_mapping_tables(_DATAPACKAGE)
-    mutated_datapackage = update_datapackage(deepcopy(_DATAPACKAGE), mappings)
+# noinspection PyShadowingNames
+def test_update_datapackage_deletes_maps_to_property(_datapackage):
+    mappings = build_mapping_tables(_datapackage)
+    mutated_datapackage = update_datapackage(deepcopy(_datapackage), mappings)
     for resource in mutated_datapackage['resources']:
         assert 'maps_to' not in resource['schema']['fields']
 
 
-def test_update_datapackage_returns_correct_mutated_dict():
-    mappings = build_mapping_tables(_DATAPACKAGE)
-    mutated_datapackage = update_datapackage(deepcopy(_DATAPACKAGE), mappings)
+# noinspection PyShadowingNames
+def test_update_datapackage_returns_correct_mutated_dict(_datapackage):
+    mappings = build_mapping_tables(_datapackage)
+    mutated_datapackage = update_datapackage(deepcopy(_datapackage), mappings)
     new_fields = mutated_datapackage['resources'][0]['schema']['fields']
     assert {'name': FISCAL_KEYS[0], 'mapped_from': 'foo'} in new_fields
     assert {'name': FISCAL_KEYS[1], 'mapped_from': 'eggs'} in new_fields
@@ -120,8 +131,9 @@ _DUMMY_ROW = {
 }
 
 
-def test_apply_mapping_returns_correct_row():
-    mappings = build_mapping_tables(_DATAPACKAGE)
+# noinspection PyShadowingNames
+def test_apply_mapping_returns_correct_row(_datapackage):
+    mappings = build_mapping_tables(_datapackage)
     assert apply_mapping(deepcopy(_DUMMY_ROW),
                          mappings=mappings,
                          resource_index=0) == {
